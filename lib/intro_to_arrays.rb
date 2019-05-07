b3fe909086028f4d52e9931018b85fc706ec55e7
Sophia Song <ososanne@gmail.com>
2019-05-07 23:46:51 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,37 @@
+'use strict';
+
+function createArray() {
+  return ["snickers", "hundred grand", "kitkat", "skittles"];
+}
+
+function addElementToArray(array) {
+  array.push("A Christmas Story");
+  return array
+}
+
+function accessElementFromArray(statesArray) {
+  return statesArray[2];
+}
+
+function replaceElementInArray(namesArray) {
+  return namesArray[1] = "Carter";
+  
+}
+
+function removeElementFromArray(dishesArray) {
+  dishesArray.splice(2, 2, "Roast Chicken");
+  return dishesArray;
+}
+
+function iterateArray(numbers) {
+  var newNums = [];
+  numbers.forEach(function(number) {
+    newNews.push(number + 5);
+  });
+  return newNews;
+}
+  })
+}
+
+
+}
